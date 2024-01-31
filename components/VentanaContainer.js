06f7738402be0e4f374a06541f9d52a9085c71a6
victor-code <vdcloayza@gmail.com>
2024-01-30 23:45:59 -0400

    version beta 0.01

--- components/VentanaContainer.js
+++ components/VentanaContainer.js
@@ -1,4 +1,24 @@
 import "./ventana_container.css";
+
+// console.log(typeof list);
+// Pass a function to map
+/* const map1 = array1
+  .map((x) => x * 2)
+  .join("")
+  .split(" ")
+  .toString();
+console.log(typeof map1);
+const inner = new DOMParser().parseFromString(list, "text/html").body.innerHTML; */
+
+const array1 = ["<p>content1</p>", "<p>content2</p>", "<p>content3</p>"];
+
+const list = document.createElement("ul");
+
+array1.forEach((element) => {
+  const listItem = document.createElement("li");
+  listItem.innerText = element;
+  list.appendChild(listItem);
+});
 export class VentanaContainer extends HTMLElement {
   constructor() {
     super();
@@ -7,13 +27,34 @@ export class VentanaContainer extends HTMLElement {
       `
         <section class="ventana_container">
         <section id="barra_de_titulo_container">
+        <div class="tab">
+        <div class="folder_icon"></div>
+        <div id="ruta_inside_title">main</div>
+        </div>
+        
         <div id="exit_btn_container">
         <span id="equis_linea_1"></span>
         <span id="equis_linea_2"></span>
         </div>
         </section>
+        ${list.innerText}
         </section>
         `;
   }
 }
 customElements.define("ventana-container", VentanaContainer);
+export const eventosVentanaContainer = (container, linea1, linea2) => {
+  ["mouseout", "mouseup", "mousedown"].forEach((event) => {
+    container.addEventListener(event, () => {
+      if (event == "mouseout") {
+        linea1.style.background = "black";
+        linea2.style.background = "black";
+      } else if (event == "mousedown") {
+        linea1.style.background = "white";
+        linea2.style.background = "white";
+      } else if (event == "mouseup") {
+        linea1.style.background = "black";
+      }
+    });
+  });
+};
--- main.js
+++ main.js
@@ -1,27 +1,8 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-
-import { setupCounter } from "./counter.js";
-import { VentanaContainer } from "./components/ventanaContainer.js";
-
-const array1 = ["<p>hola</p>", "<p>hola2</p>", "<p>hola3</p>"];
-
-const list = document.createElement("ul");
-
-array1.forEach((element) => {
-  const listItem = document.createElement("li");
-  listItem.innerText = element;
-  list.appendChild(listItem);
-});
-// console.log(typeof list);
-// Pass a function to map
-/* const map1 = array1
-  .map((x) => x * 2)
-  .join("")
-  .split(" ")
-  .toString();
-console.log(typeof map1);
-const inner = new DOMParser().parseFromString(list, "text/html").body.innerHTML; */
+import {
+  VentanaContainer,
+  eventosVentanaContainer,
+} from "./components/ventanaContainer.js";
 
 // prettier-ignore
 document.querySelector("#app").innerHTML =
@@ -29,37 +10,15 @@ document.querySelector("#app").innerHTML =
   `
   <div>
   
-  ${list.innerHTML}
   
-
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-    <ventana-container></ventana-container>
+  <ventana-container></ventana-container>
   </div>
 `;
 
+// cacheando elementos  de ventana
 const container = document.querySelector("#exit_btn_container");
 const linea1 = document.querySelector("#equis_linea_1");
 const linea2 = document.querySelector("#equis_linea_2");
+eventosVentanaContainer(container, linea1, linea2);
 
-["mouseout", "mouseup", "mousedown"].forEach((event) => {
-  container.addEventListener(event, () => {
-    if (event == "mouseout") {
-      linea1.style.background = "black";
-      linea2.style.background = "black";
-    } else if (event == "mousedown") {
-      linea1.style.background = "white";
-      linea2.style.background = "white";
-    } else if (event == "mouseup") {
-      linea1.style.background = "black";
-    }
-  });
-});
-
-
-setupCounter(document.querySelector("#counter"));
+// setupCounter(document.querySelector("#counter"));
