c9cc89c8bcdaae1f963ff82cf5538f6f416b4c1f
victor-code <vdcloayza@gmail.com>
2024-02-22 23:56:04 -0400

    apunto de aplicar proxy

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import {
   VentanaContainer,
   eventosVentanaContainer,
 } from "./components/ventanaContainer.js";
+import Router from "./services/Router.js";
 
 // prettier-ignore
 document.querySelector("#app").innerHTML =
@@ -20,5 +21,9 @@ const container = document.querySelector("#exit_btn_container");
 const linea1 = document.querySelector("#equis_linea_1");
 const linea2 = document.querySelector("#equis_linea_2");
 eventosVentanaContainer(container, linea1, linea2);
-
+window.app = {};
+app.router = Router;
 // setupCounter(document.querySelector("#counter"));
+window.addEventListener("DOMContentLoaded", () => {
+  app.router.init();
+});
