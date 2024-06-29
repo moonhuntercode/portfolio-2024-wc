986a7755f60189fcd770f1f8890ff6886db3ec4e
victor-code <vdcloayza@gmail.com>
2024-06-28 23:57:21 -0400

    version 2 oculta en beta❌

--- api/index.js
+++ api/index.js
@@ -4,23 +4,28 @@ import ViteExpress from "vite-express";
 import { engine } from "express-handlebars";
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 3051 || process.env.PORT;
+
+// config for production mode
+// ViteExpress.config({ mode: "production" });
+
 // template engine config - handlebars start
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
 // template engine config - handlebars end
+// serving  static files
+app.use(express.static("public"));
 
 app.get("/message", (_, res) => res.send("Hello from express!"));
 app.get("/home", (_, res) => {
-  res.render("home", {
-    title: "Home",
-  });
+  res.render("home");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log(`http://localhost:${port} \n
+ViteExpress.listen(app, port, () =>
+  console.log(`http://localhost:${port}/home \n
     http://localhost:${port}/message
     `)
 );
+export default app;
